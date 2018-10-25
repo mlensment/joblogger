b3d3e56a79efad1aa237dd569f5f312a1580ba09
Martin Lensment <mlensment@gmail.com>
2018-10-26 00:12:54 +0300

    Improvements

--- log.rb
+++ log.rb
@@ -1,39 +1,46 @@
+require 'time'
+
 path = File.dirname(__FILE__)
 jobs = File.read("#{path}/jobs.txt")
-puts "What are you working on?"
-puts "------------------------"
-puts jobs
-puts "------------------------"
-puts "Insert number or free text: "
-input = gets.chomp
-job = nil
+
+if ARGV[0]
+  job = ARGV[0]
+else
+  puts "What are you working on?"
+  puts "------------------------"
+  puts jobs
+  puts "------------------------"
+  puts "Insert number or free text: "
+
+  job = gets.chomp
+end
+
 jobs.split("\n").each do |x|
-  if x.start_with?(input)
+  if x.start_with?(job)
     job = x
     break
   end
 end
 
-log = File.read("#{path}/log.txt")
-running = log.split("\n").last
+log_path = "#{path}/log_#{Time.now.strftime('%Y%m')}.txt"
 
-if running
-  running.gsub!(/\d+$/, Time.now.strftime("%d%m%y%H%M%S"))
+File.write(log_path, '') unless File.file?(log_path)
+log = File.read(log_path)
+log_lines = log.split("\n")
+
+if log_lines.any?
+  running = log_lines.pop
+  start = Time.strptime(running.match(/\d+$/)[0], '%d%m%Y%H%M%S')
+  duration_in_sec = (Time.now - start).to_i
+  elapsed_time = Time.at(duration_in_sec).utc.strftime("%H:%M:%S") # use this in report
+  running.gsub!(/\d+$/, "#{start.strftime('%d.%m.%Y')} #{duration_in_sec}")
+  log_lines << running
 end
 
-job = input unless job
+log_lines << "#{job} #{Time.now.strftime("%d%m%Y%H%M%S")}"
 
-File.open("#{path}/log.txt", 'a') do |f|
-  f.write("#{running}\n") if running
-  f.write("#{job} #{Time.now.strftime("%d%m%y%H%M%S")}\n")
+File.open(log_path, 'w') do |f|
+  f.write(log_lines.join("\n"))
 end
 
-# log = File.read("#{path}/log.txt")
-# one = log[-1]
-# two = log[-2]
-
-# if one && two
-#   one.match(/\d+$/).first
-# end
-
-puts "Done!"
+puts "Successfully added new job! Last job duration was #{elapsed_time}"
